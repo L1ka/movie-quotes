41a1976db8688b5b946a95dec1be5245c8366ef4
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-02 23:27:25 +0400

    chore: migration

--- database/migrations/2023_04_01_154157_create_quotes_table.php
+++ database/migrations/2023_04_01_154157_create_quotes_table.php
@@ -4,17 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class () extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('quotes', function (Blueprint $table) {
             $table->id();
-            $table->string('username')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->text('quoteEn');
+            $table->text('quoteKa');
+            $table->string('thumbnail');
+            $table->foreignId('movie_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -24,6 +26,6 @@ return new class () extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('quotes');
     }
 };
