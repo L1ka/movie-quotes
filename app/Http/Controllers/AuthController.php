6a794a7b6497a1e67a40b3e3d2b5bbec30065aff
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-12 20:30:14 +0400

    refactor: rename storeloginrequest

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,14 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Login\StoreLoginRequest;
+use App\Http\Requests\Login\LoginRequest;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    public function signIn(StoreLoginRequest $request): RedirectResponse
+    public function signIn(LoginRequest $request): RedirectResponse
     {
         if (! auth()->attempt($request->validated())) {
             throw ValidationException::withMessages([
