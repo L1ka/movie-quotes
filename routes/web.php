2e91f6aaea2951c23fcd4c5cff3ab3565733897c
L1ka <lika.gvaramashvili@redberry.ge>
2023-04-13 15:12:45 +0400

    refactor: group routes

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,32 @@ Route::view('movies/create','movies.create')->middleware('auth')->name('movies.c
 
 Route::get('/',  [QuoteController::class, 'home'] )->name('home');
 
-Route::group(['controller' => MovieController::class, 'middleware' => 'auth'], function () {
-    Route::get('movies/dashboard',  'index')->name('movies_dashboard.index');
-    Route::post('movies/store', 'store')->name('movies.store');
-    Route::get('movies/{movie}/edit',  'edit')->name('movies.edit');
-    Route::patch('movies/{movie}',  'update')->name('movies.update');
-    Route::delete('movies/{movie}',  'destroy')->name('movies.destroy');
-    Route::get('movies/{movie}', 'list')->name('movie.list');
+Route::middleware('auth')->group(function() {
+    Route::group(['controller' => MovieController::class], function () {
+        Route::get('movies/dashboard',  'index')->name('movies_dashboard.index');
+        Route::post('movies/store', 'store')->name('movies.store');
+        Route::get('movies/{movie}/edit',  'edit')->name('movies.edit');
+        Route::patch('movies/{movie}',  'update')->name('movies.update');
+        Route::delete('movies/{movie}',  'destroy')->name('movies.destroy');
+        Route::get('movies/{movie}', 'list')->name('movie.list');
+    });
+
+    Route::group(['controller' => QuoteController::class], function () {
+        Route::get('quotes/create','create')->name('quotes.create');
+        Route::post('quotes/store', 'store')->name('quotes.store');
+        Route::get('quotes/{quote}/edit',  'edit')->name('quotes.edit');
+        Route::patch('quotes/{quote}',  'update')->name('quotes.update');
+        Route::delete('quotes/{quote}',  'destroy')->name('quotes.destroy');
+    });
 });
 
-Route::group(['controller' => QuoteController::class, 'middleware' => 'auth'], function () {
-    Route::get('quotes/create','create')->name('quotes.create');
-    Route::post('quotes/store', 'store')->name('quotes.store');
-    Route::get('quotes/{quote}/edit',  'edit')->name('quotes.edit');
-    Route::patch('quotes/{quote}',  'update')->name('quotes.update');
-    Route::delete('quotes/{quote}',  'destroy')->name('quotes.destroy');
-});
 
 Route::get('login', [AuthController::class, 'index'])->middleware('guest')->name('login.index');
 Route::post('login', [AuthController::class, 'signIn'])->middleware('guest')->name('login.sign-in');
 
 Route::get('set-locale/{locale}', [LocaleController::class, 'setLocale'])->name('set-locale');
+
+
 
 
 
